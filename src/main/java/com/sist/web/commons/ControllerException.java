b96fcf520e5696b5f331b7a232d283dcc78b84ea
dasolson <you@example.com>
2025-12-11 17:09:38 +0900

    first

---
+++ src/main/java/com/sist/web/commons/ControllerException.java
@@ -0,0 +1,31 @@
+package com.sist.web.commons;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+// @RestController는 처리 할 수 없다
+import org.springframework.web.bind.annotation.ExceptionHandler;
+/*
+ * 				object
+ *                |
+ *            Throwable
+ *                |
+ *       -------------------
+ *       |                 |
+ *     Error           Exception               
+ */
+@ControllerAdvice
+// 모든 요청기능 처리 => catch 사용
+public class ControllerException {
+	@ExceptionHandler(Exception.class)
+	public void exception(Exception ex) {
+		System.out.println("===== Controller에서 예외 발생 =====");
+		ex.printStackTrace();
+		System.out.println("=================================");
+	}
+	
+	@ExceptionHandler(Throwable.class)
+	public void throwable(Throwable ex) {
+		System.out.println("===== Controller에서 에러 발생 =====");
+		ex.printStackTrace();
+		System.out.println("=================================");
+	}
+}
---
+++ src/main/java/com/sist/web/service/FoodServiceImpl.java
@@ -0,0 +1,45 @@
+package com.sist.web.service;
+
+import org.springframework.stereotype.Service;
+import java.util.*;
+import com.sist.web.mapper.*;
+import com.sist.web.vo.*;
+
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
+/*
+ * 		@Autowired
+ * 		private final FoodMapper mapper;
+ * 
+ * 		private FoodMapper mapper
+ * 		@Autowired
+ * 		public FoodServiceImpl (FoodMapper mapper){
+ * 			this.mapper = mapper;
+ * 		}
+ * 
+ * 		==> @RequiredArgsConstructor
+ */
+public class FoodServiceImpl implements FoodService{
+	private final FoodMapper mapper;
+
+	@Override
+	public List<FoodVO> foodListData(int start) {
+		// TODO Auto-generated method stub
+		return mapper.foodListData(start);
+	}
+
+	@Override
+	public int foodTotalPage() {
+		// TODO Auto-generated method stub
+		return mapper.foodTotalPage();
+	}
+
+	@Override
+	public FoodVO foodDetailData(int fno) {
+		// TODO Auto-generated method stub
+		mapper.hitIncrement(fno);
+		return mapper.foodDetailData(fno);
+	}	
+	
+}
