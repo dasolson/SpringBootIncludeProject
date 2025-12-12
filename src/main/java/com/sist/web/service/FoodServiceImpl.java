02523ad8b486dd3e9406043e22652df249057e8b
dasolson <you@example.com>
2025-12-12 17:01:03 +0900

    20251212

--- src/main/java/com/sist/web/service/FoodServiceImpl.java
+++ src/main/java/com/sist/web/service/FoodServiceImpl.java
@@ -40,6 +40,18 @@ public class FoodServiceImpl implements FoodService{
 		// TODO Auto-generated method stub
 		mapper.hitIncrement(fno);
 		return mapper.foodDetailData(fno);
+	}
+
+	@Override
+	public List<FoodVO> foodFindData(Map map) {
+		// TODO Auto-generated method stub
+		return mapper.foodFindData(map);
+	}
+
+	@Override
+	public int foodFindTotalPage(Map map) {
+		// TODO Auto-generated method stub
+		return mapper.foodFindTotalPage(map);
 	}	
 	
 }
